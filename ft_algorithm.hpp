e37ac85175934f6da89d6407cd70bf412e8a87f0
Mohamed ELazhari <mazhari@student.1337.ma>
2023-01-31 19:01:29 +0100

    adding default lexicographical_compare function in ft_algorithm

--- ft_algorithm.hpp
+++ ft_algorithm.hpp
@@ -1,2 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_algorithm.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mazhari <mazhari@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 15:46:07 by mazhari           #+#    #+#             */
+/*   Updated: 2023/01/31 18:59:55 by mazhari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef FT_ALGORITHM_HPP
+# define FT_ALGORITHM_HPP
+
+namespace ft {
+
+	template <class InputIterator1, class InputIterator2>  
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2){
+		while (first1!=last1) {
+			if (*first1 != *first2)
+				return false;
+			++first1; 
+			++first2;
+		}
+		return true;
+	}
+
+	template <class InputIterator1, class InputIterator2, class BinaryPredicate>  
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred){
+		while (first1!=last1) {
+			if (!pred(*first1,*first2))
+				return false;
+			++first1; 
+			++first2;
+		}
+		return true;
+	}
+
+	template <class InputIterator1, class InputIterator2>
+	bool lexicographical_compare (	InputIterator1 first1, InputIterator1 last1,
+									InputIterator2 first2, InputIterator2 last2){
+			while (first1 != last1)
+			{
+				if (first2 == last2 || *first2 < *first1) 
+						return false;
+				else if (*first1 < *first2) 
+						return true;
+				++first1; 
+				++first2;
+			}
+			return ( first2!=last2 );      
+	}
+
+}
 
 # endif
