b6a55b9ce2b02cb8cd5de252aaa370fc94488700
Mohamed Azhari <mazhari@e1r5p3.1337.ma>
2023-01-31 18:53:05 +0100

    adding equal function in ft_algorithm

---
+++ ft_algorithm.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_algorithm.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mazhari <mazhari@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 15:46:07 by mazhari           #+#    #+#             */
+/*   Updated: 2023/01/31 18:28:22 by mazhari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef FT_ALGORITHM_HPP
+# define FT_ALGORITHM_HPP
+
+namespace ft {
+
+	template <class InputIterator1, class InputIterator2>  
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2){
+		while (first1!=last1) {
+			if (*first1 != *first2)
+				return false;
+			++first1; 
+			++first2;
+		}
+		return true;
+	}
+}
+
+# endif
