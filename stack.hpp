c92f6366973237eeb062e185e771d73622cb73de
Mohamed ELazhari <mazhari@student.1337.ma>
2023-02-01 22:36:38 +0100

    adding constructor to stack

--- stack.hpp
+++ stack.hpp
@@ -6,7 +6,7 @@
 /*   By: mazhari <mazhari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 22:15:54 by mazhari           #+#    #+#             */
-/*   Updated: 2023/02/01 22:27:24 by mazhari          ###   ########.fr       */
+/*   Updated: 2023/02/01 22:36:06 by mazhari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,15 @@ namespace ft {
 			typedef T   		value_type;
 			typedef Container	container_type;
 			typedef size_t		size_type;
+			//constructor
+			explicit stack (const container_type& ctnr = container_type()) : _ctnr(ctnr) {
+				return ;
+			}
+		// Member functions
 
+
+			private:
+			container_type _ctnr;
 	};
 
 }
