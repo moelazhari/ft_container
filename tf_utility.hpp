dabf11999d0d45b17f3b5c6c8aeac1772742fad4
Mohamed ELazhari <mazhari@student.1337.ma>
2023-02-02 18:46:19 +0100

    adding operator= to pair class

--- tf_utility.hpp
+++ tf_utility.hpp
@@ -6,7 +6,7 @@
 /*   By: mazhari <mazhari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 18:31:52 by mazhari           #+#    #+#             */
-/*   Updated: 2023/02/02 18:38:05 by mazhari          ###   ########.fr       */
+/*   Updated: 2023/02/02 18:45:46 by mazhari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,41 @@
 
 namespace ft {
 
-template <class T1, class T2> 
-struct pair{
-    // member types
-        typedef T1 first_type;
-        typedef T2 second_type;
-    // constructors
-        // default constructor
-        pair(): first(T1()), second(T2()){
-            return ;
-        }
-        // copy constructor
-        template<class U, class V>
-        pair(const pair<U, V>& pr): first(pr.first), second(pr.second){
-            return ;
-        }
-        // initialization constructor
-        pair(const first_type& a, const second_type& b): first(a), second(b){
-            return ;
-        }
-        
+	template <class T1, class T2> 
+	struct pair{
+		public:
+		// member types
+			typedef T1 first_type;
+			typedef T2 second_type;
+		// constructors
+			// default constructor
+			pair(): first(T1()), second(T2()){
+				return ;
+			}
+			// copy constructor
+			template<class U, class V>
+			pair(const pair<U, V>& pr): first(pr.first), second(pr.second){
+				return ;
+			}
+			// initialization constructor
+			pair(const first_type& a, const second_type& b): first(a), second(b){
+				return ;
+			}
+		// member functions
+			pair& operator=(const pair& pr){
+				this->first = pr.first;
+				this->second = pr.second;
+				return *this;
+			}
+			
+		// attributes
+		private:
+			first_type  first;
+			second_type second;
+	};
+	// relational operators overloads
 
-       private:
-        first_type  first;
-        second_type second; 
-};
-
-
+	
 
 }
 
