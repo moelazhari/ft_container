2bcd0fe06e286381526d7b7a6e771004d3d152f2
Mohamed ELazhari <mazhari@student.1337.ma>
2023-02-02 18:34:16 +0100

    creating pair class and adding her member type

---
+++ tf_utility.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tf_utility.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mazhari <mazhari@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 18:31:52 by mazhari           #+#    #+#             */
+/*   Updated: 2023/02/02 18:32:59 by mazhari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TF_UTILITY_HPP
+#define TF_UTILITY_HPP
+
+namespace ft {
+
+template <class T1, class T2> 
+struct pair{
+    // member types
+        typedef T1 first_type;
+        typedef T2 second_type;
+};
+
+
+
+}
+
+#endif
