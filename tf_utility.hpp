9aa5ef95ea9978102881072fb87779ff9f1d84ba
Mohamed ELazhari <mazhari@student.1337.ma>
2023-02-02 18:40:27 +0100

    adding constructors and attributes to pair class

--- tf_utility.hpp
+++ tf_utility.hpp
@@ -6,7 +6,7 @@
 /*   By: mazhari <mazhari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 18:31:52 by mazhari           #+#    #+#             */
-/*   Updated: 2023/02/02 18:32:59 by mazhari          ###   ########.fr       */
+/*   Updated: 2023/02/02 18:38:05 by mazhari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,25 @@ struct pair{
     // member types
         typedef T1 first_type;
         typedef T2 second_type;
+    // constructors
+        // default constructor
+        pair(): first(T1()), second(T2()){
+            return ;
+        }
+        // copy constructor
+        template<class U, class V>
+        pair(const pair<U, V>& pr): first(pr.first), second(pr.second){
+            return ;
+        }
+        // initialization constructor
+        pair(const first_type& a, const second_type& b): first(a), second(b){
+            return ;
+        }
+        
+
+       private:
+        first_type  first;
+        second_type second; 
 };
 
 
